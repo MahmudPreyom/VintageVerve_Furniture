e325fbd119a250d91d6ca34c53fd7765b200514a
Mahmud Preyom <preyom5076@gmail.com>
2024-06-03 06:27:01 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+        float: 'float 2s ease-in-out infinite',
+      },keyframes: {
+        float: {
+          '0%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-5px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },},},
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('daisyui'),
+  ],
+}
